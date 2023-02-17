5935325af475bfd7ad690fad9bc740333c902ac0
acep nurman sidik <acepnurmansidik@gmail.com>
2023-02-17 09:39:33 +0700

    refactor code, migrate  to database

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { TaskModel } from './tasks.model';
+import { TaskStatus } from './tasks-status.enum';
 import {
   Body,
   Controller,
@@ -14,54 +14,53 @@ import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
-
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
-  @Get()
-  getAllTasks(@Query() getFilterDto: GetTaskFilterDto): TaskModel[] {
-    if (Object.keys(getFilterDto).length) {
-      return this.tasksService.getAllTaskWithFilter(getFilterDto);
-    } else {
-      return this.tasksService.getAllTasks();
-    }
-  }
+  // @Get()
+  // getAllTasks(@Query() getFilterDto: GetTaskFilterDto): TaskModel[] {
+  //   if (Object.keys(getFilterDto).length) {
+  //     return this.tasksService.getAllTaskWithFilter(getFilterDto);
+  //   } else {
+  //     return this.tasksService.getAllTasks();
+  //   }
+  // }
 
-  @Get('/:id')
-  getDetailTask(@Param('id') id: string) {
-    // jika id nya ada
-    const found = this.tasksService.getDetailTask(id);
-    // jika tidak ada kirim response 404
-    if (!found) {
-      throw new NotFoundException(`Task with "${id}" not found`);
-    }
+  // @Get('/:id')
+  // getDetailTask(@Param('id') id: string) {
+  //   // jika id nya ada
+  //   const found = this.tasksService.getDetailTask(id);
+  //   // jika tidak ada kirim response 404
+  //   if (!found) {
+  //     throw new NotFoundException(`Task with "${id}" not found`);
+  //   }
 
-    return found;
-  }
+  //   return found;
+  // }
 
-  @Post()
-  createTask(@Body() createTaskDTO: CreateTaskDto): TaskModel {
-    // ambil body yang dikirim dari FE
-    return this.tasksService.createTask(createTaskDTO);
-  }
+  // @Post()
+  // createTask(@Body() createTaskDTO: CreateTaskDto): TaskModel {
+  //   // ambil body yang dikirim dari FE
+  //   return this.tasksService.createTask(createTaskDTO);
+  // }
 
-  @Delete('/:id')
-  destroyTaskByID(@Param('id') id: string): TaskModel {
-    const found = this.tasksService.destroyTask(id);
-    if (!found) {
-      throw new NotFoundException(`Task with "${id}" not found`);
-    }
+  // @Delete('/:id')
+  // destroyTaskByID(@Param('id') id: string): TaskModel {
+  //   const found = this.tasksService.destroyTask(id);
+  //   if (!found) {
+  //     throw new NotFoundException(`Task with "${id}" not found`);
+  //   }
 
-    return found;
-  }
+  //   return found;
+  // }
 
-  @Patch('/:id')
-  updateByID(
-    @Param('id') id: string,
-    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
-  ): TaskModel {
-    const { status } = updateTaskStatusDto;
-    return this.tasksService.updateTask(id, status);
-  }
+  // @Patch('/:id')
+  // updateByID(
+  //   @Param('id') id: string,
+  //   @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+  // ): TaskModel {
+  //   const { status } = updateTaskStatusDto;
+  //   return this.tasksService.updateTask(id, status);
+  // }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,72 +1,58 @@
 import { Injectable } from '@nestjs/common';
-import { TaskModel, TaskStatus } from './tasks.model';
+import { TaskStatus } from './tasks-status.enum';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskFilter } from './dto/filter.dto';
 
 @Injectable()
 export class TasksService {
-  private tasks: TaskModel[] = [];
-
-  getAllTasks(): TaskModel[] {
-    return this.tasks;
-  }
-
-  getAllTaskWithFilter(filterQuery: TaskFilter): TaskModel[] {
-    // destructure
-    const { search, status } = filterQuery;
-    let newTask = this.getAllTasks();
-
-    if (status) {
-      newTask = newTask.filter((task) => task.status !== status);
-    }
-
-    if (search) {
-      newTask = newTask.filter((task) => {
-        if (task.title.includes(search) || task.description.includes(search)) {
-          return true;
-        }
-
-        return false;
-      });
-    }
-
-    return newTask;
-  }
-
-  getDetailTask(id: string): TaskModel {
-    return this.tasks.find((rs) => rs.id === id);
-  }
-
-  createTask(createTaskDTO: CreateTaskDto): TaskModel {
-    // spred parameter
-    const { title, description } = createTaskDTO;
-    // simpan form json e dalam objek
-    const task: TaskModel = {
-      id: uuid(),
-      title,
-      description,
-      status: TaskStatus.DONE,
-    };
-
-    // push datanya ke array
-    this.tasks.push(task);
-
-    // return nilai yang dikirim
-    return task;
-  }
-
-  destroyTask(id: string): TaskModel {
-    const newTask = this.tasks.find((itemTask) => itemTask.id == id);
-    this.tasks = this.tasks.filter((itemTask) => itemTask.id != id);
-    return newTask;
-  }
-
-  updateTask(id: string, status: TaskStatus): TaskModel {
-    // rest parameter
-    const task = this.getDetailTask(id);
-    task.status = status;
-
-    return task;
-  }
+  // getAllTasks(): TaskModel[] {
+  //   return this.tasks;
+  // }
+  // getAllTaskWithFilter(filterQuery: TaskFilter): TaskModel[] {
+  //   // destructure
+  //   const { search, status } = filterQuery;
+  //   let newTask = this.getAllTasks();
+  //   if (status) {
+  //     newTask = newTask.filter((task) => task.status !== status);
+  //   }
+  //   if (search) {
+  //     newTask = newTask.filter((task) => {
+  //       if (task.title.includes(search) || task.description.includes(search)) {
+  //         return true;
+  //       }
+  //       return false;
+  //     });
+  //   }
+  //   return newTask;
+  // }
+  // getDetailTask(id: string): TaskModel {
+  //   return this.tasks.find((rs) => rs.id === id);
+  // }
+  // createTask(createTaskDTO: CreateTaskDto): TaskModel {
+  //   // spred parameter
+  //   const { title, description } = createTaskDTO;
+  //   // simpan form json e dalam objek
+  //   const task: TaskModel = {
+  //     id: uuid(),
+  //     title,
+  //     description,
+  //     status: TaskStatus.DONE,
+  //   };
+  //   // push datanya ke array
+  //   this.tasks.push(task);
+  //   // return nilai yang dikirim
+  //   return task;
+  // }
+  // destroyTask(id: string): TaskModel {
+  //   const newTask = this.tasks.find((itemTask) => itemTask.id == id);
+  //   this.tasks = this.tasks.filter((itemTask) => itemTask.id != id);
+  //   return newTask;
+  // }
+  // updateTask(id: string, status: TaskStatus): TaskModel {
+  //   // rest parameter
+  //   const task = this.getDetailTask(id);
+  //   task.status = status;
+  //   return task;
+  // }
 }
