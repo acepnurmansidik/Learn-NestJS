139025eb5b6a18715fd481f4332b7b6eca366be0
acep nurman sidik <acepnurmansidik@gmail.com>
2023-02-21 10:22:13 +0700

    added signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersRepository } from './users.repository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,5 +13,18 @@ export class AuthService {
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    // desctructure
+    const { username, password } = authCredentialsDto;
+    // cari username di database
+    const user = await this.usersRepository.findOneBy({ username });
+    // cek usernya dan komparasikan password yang diinput dengan di database
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'mission success';
+    } else {
+      throw new UnauthorizedException('Please check your login credentials');
+    }
   }
 }
